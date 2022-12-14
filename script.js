ba32811eef5ad5b26c868e1c98d02cc77b91f288
raf-zig <ziravin@yandex.ru>
2022-12-14 21:15:18 +0500

    Create the css file and the cycle of creating div in the js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+for (let i = 0; i < 16; i++){
+    const div = document.createElement('div'); 
+    div.setAttribute('id', 'con');
+    container.appendChild(div);
+};
+
